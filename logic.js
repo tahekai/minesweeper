f9cea5a7332cc51b270658886fe9e204c416d4ac
tahekai <tahekai@gmail.com>
2017-10-25 11:44:40 +0300

    25.10

--- logic.js
+++ logic.js
@@ -1,34 +1,38 @@
-//x-rida
-//y=tulp
-
 var size;
 var bombs;
 var board = [];
 var moves;
+var gameOn;
 
 
-<!--LAUA MASSIIV JA POMMID-->
-
-// returns board as list of lists like [[0,1],[1,0]]
-// size is the row/col length, bombs the number of bombs to insert
-// NB! This algorithm may become slow if the nr of bombs is close to
-// the board area (i.e. almost all the board covered with bombs)
-//
+function alusta() {
+    document.getElementById("gameBoard").innerHTML = "";
+    document.getElementById("msg").innerHTML = "";
+    moves = 0;
+    createTable();
+    
+}
 
 function createTable() {
-    
-    document.getElementById("gameBoard").innerHTML = "";
-    moves = 0;
-    document.getElementById("msg").innerHTML = "";
     size = document.getElementById('grid').value;
     bombs = document.getElementById('numOfMines').value;
+    
+    if (bombs == "" || bombs == 0 ) {
+        alert("siseta pommide arv");
+        return;
+    }
+    else if (bombs>=size*size) {
+        alert("liiga palju pomme");
+        return;
+    }
+    
     makeBoard(size, bombs);
+ 
     console.log("Mängulaua suurus: " + size);
     console.log("Pommide arv mängulaual: " + bombs);
     
     var place = document.getElementById('gameBoard');
     var table = document.createElement('table');
-    table.setAttribute("id", "table");
     var tableBody = document.createElement('tbody');
     for (var x = 0; x < parseInt(size,10); x++){
         var tableRow = document.createElement('tr');
@@ -44,31 +48,73 @@ function createTable() {
     }
     table.appendChild(tableBody);
     place.appendChild(table);
-    //place.innerHTML = table;
-    //tableCell.addEventListener("click", clickCell);
+    
+    if (size == 6) {
+        table.setAttribute("class", "six");
+        tableCell.setAttribute("class", "tdSix")
+    }
+    if (size == 9) {
+        table.setAttribute("class", "nine")
+        tableCell.setAttribute("class", "tdNine")
+    }
+    if (size == 16) {
+        table.setAttribute("class", "sixteen")
+        tableCell.setAttribute("class", "tdSixteen")
+    }
+    gameOn = true;
+}
+
+function makeBoard(size,bombs) {
+    
+    //if (bombs>=size*size) throw "too many bombs for this size";
+    //if (bombs == "") throw "missing amount of bombs";
+    //if (bombs == 0) throw "missing amount of bombs";
+    
+   
+  
+    // initialize board, filling with zeros
+    for (var x=0; x<size; x++) {
+        board[x]=[]; // insert empty subarray
+        for (var y=0; y<size; y++) board[x][y]=0;
+    }
+
+    // now fill board with bombs in random positions
+    var i=bombs;
+    while (i>0) {
+        // generate random x and y in range 0...size-1
+        var x=Math.floor(Math.random() * size);
+        var y=Math.floor(Math.random() * size);
+        // put bomb on x,y unless there is a bomb already
+        if (board[x][y]!=1) {
+          board[x][y]=1;
+          i--; // bomb successfully positioned, one less to go
+          console.log("positioned "+x+", "+y+" yet to go "+i); 
+        }
+    }  
+  return board;
+  
 }
 
 function clickCell(cell) {
+    
+    if (gameOn == false) return;
+    if (cell.target.innerHTML !="")return;
     moves++;
-    document.getElementById("msg").innerHTML = moves;
+    document.getElementById("msg").innerHTML = "Käike: " + moves;
     var clickedCell = cell.target.id;
     var x = clickedCell.substring(0,clickedCell.search("_"));
     var y = clickedCell.substring(clickedCell.search("_")+1);
     document.getElementById(x+"_"+y).setAttribute("style", "background-color: white");
-    console.log("Sinu käik: " + x +  " ja " + y);
-    console.log(neighbours(size, x, y));
+    //console.log("Sinu käik: " + x +  " ja " + y);
+    //console.log(neighbours(size, x, y));
     var z = neighbours(size, x, y);
     console.log(countBombs(z));
-    makeMove(x, y);
-}
-
-function makeMove(x, y) {
     
     if (board[x][y] == 0){
         console.log("ei ole pomm");
         var z = neighbours(size, x, y);
         console.log(countBombs(z));
-        document.getElementById(x + "_" + y ).innerHTML = countBombs(z);
+        document.getElementById(x + "_" + y ).innerHTML = countBombs(z); 
         
     }
     else if (board[x][y] == 1) {
@@ -82,98 +128,62 @@ function makeMove(x, y) {
         }
         
         document.getElementById("msg").innerHTML = "Kaotus, käike tegid: " + moves;
+        gameOn = false;
         
     }
-}
-
-function makeBoard(size,bombs) {
-  //var board = [];
-  
-  //console.log("makeboard");
-  
-  if (bombs>=size*size) throw "too many bombs for this size";
-  if (bombs == "")  throw "missing amount of bombs";
-  
-  // initialize board, filling with zeros
-  for (var x=0; x<size; x++) {
-    board[x]=[]; // insert empty subarray
-    for (var y=0; y<size; y++) board[x][y]=0;
-  }
-
-  // now fill board with bombs in random positions
-  var i=bombs;
-  while (i>0) {
-    // generate random x and y in range 0...size-1
-    var x=Math.floor(Math.random() * size);
-    var y=Math.floor(Math.random() * size);
-    // put bomb on x,y unless there is a bomb already
-    if (board[x][y]!=1) {
-      board[x][y]=1;
-      i--; // bomb successfully positioned, one less to go
-      console.log("positioned "+x+", "+y+" yet to go "+i); 
-    }
-  }  
-  return board;
+    if (size*size - bombs == moves) {
+            document.getElementById("msg").innerHTML = "Võit, käike tegid: " + moves;
+         gameOn = false;
+        }
+   
 }
 
 
 function neighbours(size,y,x) {
-x = parseInt(x);
-y = parseInt(y);    
-var list=[];
-  for (var i=-1; i<=1; i++) {    
-    for (var j=-1; j<=1; j++) {
-      // square is not a neighbour of itself
-      if (i==0 && j==0) continue;
-      // check whether the the neighbour is inside board bounds
-      if ((x+i)>=0 && (x+i)<size && (y+j)>=0 && (y+j)<size) {
-        list.push([x+i,y+j]);  
+    
+    x = parseInt(x);
+    y = parseInt(y);    
+    var list=[];
+      for (var i=-1; i<=1; i++) {    
+        for (var j=-1; j<=1; j++) {
+          // square is not a neighbour of itself
+          if (i==0 && j==0) continue;
+          // check whether the the neighbour is inside board bounds
+          if ((x+i)>=0 && (x+i)<size && (y+j)>=0 && (y+j)<size) {
+            list.push([x+i,y+j]);  
+          }
+        }
       }
-    }
-  }
   return list;
   
 }  
 
 function countBombs(array) {
     
-    var totBombs = 0;
+    var numOfBombs = 0;
     for (var i = 0; i < array.length; i++){
          //console.log(array[i][1]+ "_" + array[i][0]);
         if (board[array[i][1]][array[i][0]] == 1)
         {
-            totBombs++;
+            numOfBombs++;
         }
     }
-    return totBombs;
+    return numOfBombs;
 }
 
-
-
-
-function reload() {
-    window.location.reload();
-}
-
-
-/*function draw(x,y) {
-    size = document.getElementById('grid').value;
+/*function saveGame() {
+    var player, game,url;
     
-    bombs = document.getElementById('numOfMines').value;
-    var board = makeBoard(size, bombs);
-    console.log("Size of board " + size);
-    console.log("Number of mines " + bombs);
+    player=document.getElementById("nimi").value; //tee input field kust saab mängija nime
+    game=JSON.stringify(board);
+    console.log(player);
+    console.log(game);
     
-    for(var x=0;x<parseInt(size,10) ;x++){
-        var tableRow=document.getElementById('gameBoard').insertRow(x);
-        tableRow.id = tableRow.rowIndex;
-        for(var y = 0; y < parseInt(size,10); y++){
-            var tableCell=  tableRow.insertCell(y);
-            y.innerHTML="Row-"+x+" Column-"+y;
-            tableCell.id = tableRow.id + "_" + tableCell.cellIndex;
-          
-            
-        }
-    }
+    url=
+    url+="?op=store&laud="+game+"&mangija="+player;
+    fetch(url).then(r=>r.text()).then(handlestore)
 }
-*/
+
+function handlestore(x) {
+    console.log("got it");
+}*/
